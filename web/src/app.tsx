31608a7f977924c46534fc41af3afbddfb7f66c9
Andrea <andrea.n96@hotmail.it>
2026-04-03 10:30:29 +0200

    Refactoring

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,7 +2,6 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-
 import Navbar from "@components/common/Navbar";
 
 import "@styles/global.css";

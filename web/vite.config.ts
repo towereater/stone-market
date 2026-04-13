54deb2f2da4052105fd3216c9d5b0aadd2a9cbf5
Andrea <andrea.n96@hotmail.it>
2026-04-13 23:32:22 +0200

    Added path for layouts folder

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       '@images': path.resolve(__dirname, './src/assets/images'),
       '@styles': path.resolve(__dirname, './src/assets/styles'),
       '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@store': path.resolve(__dirname, './src/store'),
       '@classes': path.resolve(__dirname, './src/classes'),

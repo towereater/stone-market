ca270810b97cb72610f9541557c7107a066bceef
Andrea <andrea.n96@hotmail.it>
2026-04-13 22:58:46 +0200

    Moved navbar parent component

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,7 +2,6 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Navbar from "@components/common/Navbar";
 
 import "@styles/global.css";
 
@@ -12,7 +11,6 @@ export default function App() {
       root={props => (
         <MetaProvider>
           <Title>Marble Market</Title>
-          <Navbar></Navbar>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Component } from "solid-js";
 import Hero from "@components/features/Hero";
 import FeaturedSales from "@components/features/FeaturedSales";
+import Navbar from "@components/common/Navbar";
 
 const Home: Component = () => {
   return (
     <>
+      <Navbar></Navbar>
       <Hero />
       <FeaturedSales />
     </>

bdba56ecbac851793dee4a46d60bd205f1d226e9
Andrea <andrea.n96@hotmail.it>
2026-04-13 23:33:23 +0200

    Replaced routing system with layouts

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,14 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
+import { Router, Route } from "@solidjs/router";
 import { Suspense } from "solid-js";
+
+import MainLayout from "@layouts/MainLayout";
+import AccessLayout from "@layouts/AccessLayout";
+import ProfileLayout from "@layouts/ProfileLayout";
+
+import Home from "@pages/Home";
+import Login from "@pages/access/Login";
+import Register from "@pages/access/Register";
 
 import "@styles/global.css";
 
@@ -15,7 +22,20 @@ export default function App() {
         </MetaProvider>
       )}
     >
-      <FileRoutes />
+      <Route path="/" component={MainLayout}>
+        <Route path="/" component={Home} />
+      </Route>
+
+      <Route path="/login" component={AccessLayout}>
+        <Route path="/" component={Login} />
+      </Route>
+      <Route path="/register" component={AccessLayout}>
+        <Route path="/" component={Register} />
+      </Route>
+
+      <Route path="/profile" component={ProfileLayout}>
+        
+      </Route>
     </Router>
   );
 }

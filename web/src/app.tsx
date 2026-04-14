b7f4215ebf29a68f0b40eb370c52fe65dfb711c8
Andrea <andrea.n96@hotmail.it>
2026-04-14 17:57:09 +0200

    Changed routing system

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,18 +1,8 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import { Router, Route } from "@solidjs/router";
+import { Router } from "@solidjs/router";
 import { Suspense } from "solid-js";
 
-import MainLayout from "@layouts/MainLayout";
-import AccessLayout from "@layouts/AccessLayout";
-import ProfileLayout from "@layouts/ProfileLayout";
-
-import Home from "@pages/Home";
-import Login from "@pages/access/Login";
-import Register from "@pages/access/Register";
-import CreateListing from "./pages/CreateListing";
-import Buy from "./pages/Buy";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
+import { routes } from "./routes";
 
 import "@styles/global.css";
 
@@ -26,29 +16,7 @@ export default function App() {
         </MetaProvider>
       )}
     >
-      <Route path="/" component={MainLayout}>
-        <Route path="/" component={Home} />
-        <Route path="/create-listing" component={CreateListing} />
-        <Route path="/buy" component={Buy} />
-        <Route path="/about" component={About} />
-        <Route path="*404" component={NotFound} />
-      </Route>
-
-      <Route path="/login" component={AccessLayout}>
-        <Route path="/" component={Login} />
-      </Route>
-      <Route path="/register" component={AccessLayout}>
-        <Route path="/" component={Register} />
-      </Route>
-
-      <Route path="/profile" component={ProfileLayout}>
-        <Route path="/" component={Register} />
-        <Route path="/active-listings" component={Register} />
-        <Route path="/received-orders" component={Register} />
-        <Route path="/history" component={Register} />
-        <Route path="/active-orders" component={Register} />
-        <Route path="/orders-history" component={Register} />
-      </Route>
+      {routes}
     </Router>
   );
 }
---
+++ web/src/routes.ts
@@ -0,0 +1,69 @@
+import { lazy } from "solid-js";
+import type { RouteDefinition } from "@solidjs/router";
+
+import MainLayout from "@layouts/MainLayout";
+import AccessLayout from "@layouts/AccessLayout";
+import ProfileLayout from "@layouts/ProfileLayout";
+
+export const routes: RouteDefinition[] = [
+  {
+    path: "/",
+    component: MainLayout,
+    children: [
+        {
+            path: "/",
+            component: lazy(() => import("@pages/Home")),
+        },
+        {
+            path: "/create-listing",
+            component: lazy(() => import("@pages/CreateListing")),
+        },
+        {
+            path: "/buy",
+            component: lazy(() => import("@pages/Buy")),
+        },
+        {
+            path: "/about",
+            component: lazy(() => import("@pages/About")),
+        },
+        {
+            path: "*404",
+            component: lazy(() => import("@pages/NotFound")),
+        }
+    ]
+  },
+  {
+    path: "/login",
+    component: AccessLayout,
+    children: [
+        {
+            path: "/",
+            component: lazy(() => import("@pages/access/Login")),
+        }
+    ]
+  },
+  {
+    path: "/register",
+    component: AccessLayout,
+    children: [
+        {
+            path: "/",
+            component: lazy(() => import("@pages/access/Register")),
+        }
+    ]
+  },
+  {
+    path: "/profile",
+    component: ProfileLayout,
+    children: [
+      {
+        path: "/",
+        component: lazy(() => import("@pages/profile/Profile")),
+      },
+      {
+        path: "/history",
+        component: lazy(() => import("@pages/profile/ListingsHistory")),
+      }
+    ]
+  }
+];

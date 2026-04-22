143511c4208d2bfd1376dd421bba63991fb7e209
Andrea <andrea.n96@hotmail.it>
2026-04-22 12:57:03 +0200

    Moved styles to source code

--- web/src/components/ui/NavbarLink.tsx
+++ web/src/components/ui/NavbarLink.tsx
@@ -1,8 +1,6 @@
 import { Component, JSX, splitProps } from "solid-js";
 import { A } from "@solidjs/router";
 
-import styles from "@styles/NavbarLink.module.css";
-
 interface NavbarLinkProps extends JSX.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
 }
@@ -11,7 +9,7 @@ export const NavbarLink: Component<NavbarLinkProps> = (props) => {
   const [local, _] = splitProps(props, ["href", "children", "class"]);
 
   return (
-    <A href={local.href} class={styles.link}>
+    <A href={local.href} class="hover:text-[#c2c9cc] transition-colors">
       {local.children}
     </A>
   );

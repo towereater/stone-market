41c85568e3bd5edaf81519448554826dadb11f8b
Andrea <andrea.n96@hotmail.it>
2026-04-23 17:10:35 +0200

    Added session and user services

---
+++ web/src/lib/services/session.ts
@@ -0,0 +1,19 @@
+export const sessionService = {
+  createSession: async (username: string, password: string) => {
+    const response = await fetch(`${import.meta.env.VITE_API_HOST}/session`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+      throw new Error(errorData.message || "Invalid credentials");
+    }
+
+    return response.json();
+  },
+};
---
+++ web/src/lib/services/user.ts
@@ -0,0 +1,20 @@
+export const userService = {
+  createUser: async (name: string, username: string, password: string) => {
+    const response = await fetch(`${import.meta.env.VITE_API_HOST}/users`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        username,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+      throw new Error(errorData.message || "Registration error");
+    }
+
+    return response.json();
+  }
+};

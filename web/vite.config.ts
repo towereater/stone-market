cd59ebb9c1e51d270e530183876873e83a05bac8
Andrea <andrea.n96@hotmail.it>
2026-04-23 11:47:33 +0200

    Moved store folder to lib

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@layouts': path.resolve(__dirname, './src/components/layouts'),
       '@classes': path.resolve(__dirname, './src/lib/classes'),
+      '@store': path.resolve(__dirname, './src/lib/store'),
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@store': path.resolve(__dirname, './src/store'),
     },
   },
 });

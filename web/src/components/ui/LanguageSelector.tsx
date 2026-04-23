8b6029fbceeb39c9b9dbb797f389cb6d273803a4
Andrea <andrea.n96@hotmail.it>
2026-04-23 11:33:41 +0200

    Created language selector component

---
+++ web/src/components/ui/LanguageSelector.tsx
@@ -0,0 +1,15 @@
+import { Component } from "solid-js";
+import { locale, setLocale, t, Locale } from "@store/i18n";
+
+export const LanguageSelector: Component = () => {
+  return (
+    <select
+      class="text-sm cursor-pointer font-medium tracking-widest text-accent"
+      value={locale()}
+      onChange={(e) => setLocale(e.currentTarget.value as Locale)}
+    >
+      <option value="en" class="text-accent">EN</option>
+      <option value="it" class="text-accent">IT</option>
+    </select>
+  );
+};
